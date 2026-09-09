50d638a9ed6b77147e8bb84126c460507abadd29
xvqingshan00 <xvqingshan98@gmail.com>
2026-09-09 17:43:45 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!，Hellow Marco！");
+        System.out.println("Hello World!，Hello Marco！");
     }
 }
